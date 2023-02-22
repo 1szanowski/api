7c3e0eba4d0d25aaab9fcd1bc6b3acd935cb1e07
Aleks Szanowski <117806276+1szanowski@users.noreply.github.com>
2023-02-22 21:32:33 +0100

    Add files via upload

--- popup.js
+++ popup.js
@@ -17,13 +17,42 @@ document.getElementById("clickMe").addEventListener("click", function() {
         let jsonData = result.data;
         if (jsonData) {
             alert("Data loaded, NASA hacking in progress!")
-            let {first,second,values} = jsonData;
-            let arr = values.map((el) => el[0]);
-            let option = "";
-            for(let i = 0; i < arr.length; i ++) {
-                option+= '<option value = "'+ arr[i] + '">' + arr[i] + "</option>"
+            console.log(jsonData);
+            // Get the dropdown element
+            let dropdown = document.getElementById("myDropdown");
+
+// Get the select element
+            let select = document.getElementById("item");
+
+// Get the data
+           let data = jsonData;
+
+// Populate the select element with the values from the data
+            for (let i = 1; i < data.values.length; i++) {
+                let option = document.createElement("option");
+                option.value = data.values[i][0];
+                option.text = data.values[i][0];
+                select.appendChild(option);
             }
-            document.getElementById("item").innerHTML = option;
+
+
+
+// Add a change event listener to the select element
+            select.addEventListener("change", function() {
+                // Get the selected value from the select element
+                let selectedValue = select.options[select.selectedIndex].value;
+
+                // Get the index of the selected value in the data array
+                let index = data.values.findIndex(function(element) {
+                    return element[0] === selectedValue;
+                });
+
+                // Display the quantity of the selected item in the <p> element
+                document.getElementById("displayData").innerHTML =  data.values[index][1];
+            });
+
+
+
         } else {
             console.log("ERROR!")
         }
